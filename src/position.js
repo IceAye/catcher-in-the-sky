ddac14421a895c4b91da3fd2cb2f1af56a10dbd9
Irene Volodko <ice7aye@gmail.com>
2025-07-17 17:30:13 +0300

    feat(position): add equals() and clone() methods

--- src/position.js
+++ src/position.js
@@ -3,4 +3,13 @@ export class Position {
     this.x = x;
     this.y = y;
   }
+
+  equals(otherPosition) {
+    if (!otherPosition) return false;
+    return this.x === otherPosition.x && this.y === otherPosition.y;
+  }
+
+  clone() {
+    return new Position(this.x, this.y);
+  }
 }
