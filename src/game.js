bf80f6b11c6cd2ddce37ea457d51436544285b8b
Irene Volodko <ice7aye@gmail.com>
2025-07-23 16:42:27 +0300

    chore(game): reorder fields and methods for ESLint-compliant class structure

--- src/game.js
+++ src/game.js
@@ -5,16 +5,161 @@ import { Glitch } from './glitch.js';
 import { GlitchSpeedJump } from './glitch-speed-jump.js';
 
 export class Game {
+  constructor(numberUtility , gameSettings) {
+    this.#numberUtility = numberUtility;
+    this.#settings = new Settings(gameSettings);
+  }
+
   #status = GAME_STATUSES.PENDING;
+
+  get status() {
+    return this.#status;
+  }
+
   #glitch;
+
+  get glitch() {
+    return this.#glitch;
+  }
+
+  get glitchPosition() {
+    return this.#glitch.position;
+  }
+
+  set glitchPosition(newPosition) {
+    this.#glitch.position = newPosition;
+  }
+
   #glitchSetIntervalId;
+
   #catchers = new Map();
+
   #catcherOne;
+
+  get catcherOne() {
+    return this.#catcherOne;
+  }
+
+  get catcherOnePosition() {
+    return this.#catcherOne.position;
+  }
+
+  set catcherOnePosition(newPosition) {
+    this.#catcherOne.position = newPosition;
+  }
+
   #catcherTwo;
+
+  get catcherTwo() {
+    return this.#catcherTwo;
+  }
+
+  get catcherTwoPosition() {
+    return this.#catcherTwo.position;
+  }
+
   #settings;
+
+  get settings() {
+    return {
+      ...this.#settings ,
+      skySize: {
+        ...this.#settings.skySize
+      } ,
+      glitchSpeedJump: {
+        ...this.#settings.glitchSpeedJump
+      } ,
+      gameTime: this.#settings.gameTime
+    };
+  }
+
+  set settings(settings) {
+    if (settings.skySize &&
+      settings.skySize.rowsCount * settings.skySize.columnsCount < 4) {
+      throw new RangeError('The size of the sky should be bigger');
+    }
+
+    const skySize = settings.skySize
+                    ? { ...settings.skySize }
+                    : this.#settings.skySize;
+
+    const glitchSpeedJump = settings.glitchSpeedJump
+                            ? new GlitchSpeedJump(settings.glitchSpeedJump.level)
+                            : this.#settings.glitchSpeedJump;
+
+    const gameTime = settings.gameTime ? settings.gameTime : this.#settings.gameTime;
+
+    this.#settings = {
+      ...this.#settings ,
+      skySize ,
+      glitchSpeedJump,
+      gameTime
+    };
+  }
+
   #score = new Map();
+
   #numberUtility;
 
+  start() {
+    if (this.#status === GAME_STATUSES.PENDING) {
+      this.#createUnits();
+      this.#status = GAME_STATUSES.IN_PROGRESS;
+    }
+
+    this.#runGlitchJumpInterval();
+  }
+
+  async stop() {
+    clearInterval(this.#glitchSetIntervalId);
+    this.#status = GAME_STATUSES.COMPLETED;
+  }
+
+  moveCatcher(catcherId , direction) {
+    const catcher = this.#catchers.get(catcherId);
+    if (!catcher) throw new Error(`Catcher with id ${catcherId} not found`);
+
+    const newPosition = catcher.position.clone();
+
+    switch (direction) {
+      case MOVE_DIRECTIONS.UP:
+        newPosition.y--;
+        break;
+      case MOVE_DIRECTIONS.DOWN:
+        newPosition.y++;
+        break;
+      case MOVE_DIRECTIONS.LEFT:
+        newPosition.x--;
+        break;
+      case MOVE_DIRECTIONS.RIGHT:
+        newPosition.x++;
+        break;
+      default:
+        throw new Error('Invalid direction');
+    }
+
+    if (!this.#isInsideSky(newPosition)) return;
+    if (this.#isCellBusyByOtherCatcher(newPosition , catcherId)) return;
+
+    catcher.position = newPosition;
+
+    //todo: ask whether is needed
+    const wasGlitchCaught = this.#isGlitchBeingCaught(catcherId);
+
+    if (wasGlitchCaught) {
+      this.#updateScore(catcherId , 15);
+    }
+
+    this.#updateGlitchStrike(catcherId , wasGlitchCaught);
+  }
+
+  getCatcherScore(catcherId) {
+    return this.#score.get(catcherId).points ?? 0;
+  }
+
+  getGlitchStrike(catcherId) {
+    return this.#score.get(catcherId)?.glitchStrike ?? 0;
+  }
 
   #glitchJump() {
     this.#glitch.position = this.#numberUtility.getRandomPosition(
@@ -91,148 +236,6 @@ export class Game {
     }
 
     this.#score.set(catcherId , updatedScore);
-  }
-
-
-  // dependency injection
-  constructor(numberUtility , gameSettings) {
-    this.#numberUtility = numberUtility;
-    this.#settings = new Settings(gameSettings);
-  }
-
-  // post
-  start() {
-    if (this.#status === GAME_STATUSES.PENDING) {
-      this.#createUnits();
-      this.#status = GAME_STATUSES.IN_PROGRESS;
-    }
-
-    this.#runGlitchJumpInterval();
-
-  }
-
-  async stop() {
-    clearInterval(this.#glitchSetIntervalId);
-    this.#status = GAME_STATUSES.COMPLETED;
-  }
-
-  moveCatcher(catcherId , direction) {
-    const catcher = this.#catchers.get(catcherId);
-    if (!catcher) throw new Error(`Catcher with id ${catcherId} not found`);
-
-    const newPosition = catcher.position.clone();
-
-    switch (direction) {
-      case MOVE_DIRECTIONS.UP:
-        newPosition.y--;
-        break;
-      case MOVE_DIRECTIONS.DOWN:
-        newPosition.y++;
-        break;
-      case MOVE_DIRECTIONS.LEFT:
-        newPosition.x--;
-        break;
-      case MOVE_DIRECTIONS.RIGHT:
-        newPosition.x++;
-        break;
-      default:
-        throw new Error('Invalid direction');
-    }
-
-    if (!this.#isInsideSky(newPosition)) return;
-    if (this.#isCellBusyByOtherCatcher(newPosition , catcherId)) return;
-
-    catcher.position = newPosition;
-
-    //todo: ask whether is needed
-    const wasGlitchCaught = this.#isGlitchBeingCaught(catcherId);
-
-    if (wasGlitchCaught) {
-      this.#updateScore(catcherId , 15);
-    }
-
-    this.#updateGlitchStrike(catcherId , wasGlitchCaught);
-  }
-
-  get status() {
-    return this.#status;
-  }
-
-  get settings() {
-    return {
-      ...this.#settings ,
-      skySize: {
-        ...this.#settings.skySize
-      } ,
-      glitchSpeedJump: {
-        ...this.#settings.glitchSpeedJump
-      } ,
-      gameTime: this.#settings.gameTime
-    };
-  }
-
-  set settings(settings) {
-    if (settings.skySize &&
-      settings.skySize.rowsCount * settings.skySize.columnsCount < 4) {
-      throw new RangeError('The size of the sky should be bigger');
-    }
-
-    const skySize = settings.skySize
-                    ? { ...settings.skySize }
-                    : this.#settings.skySize;
-
-    const glitchSpeedJump = settings.glitchSpeedJump
-                            ? new GlitchSpeedJump(settings.glitchSpeedJump.level)
-                            : this.#settings.glitchSpeedJump;
-
-    const gameTime = settings.gameTime ? settings.gameTime : this.#settings.gameTime;
-
-    this.#settings = {
-      ...this.#settings ,
-      skySize ,
-      glitchSpeedJump,
-      gameTime
-    };
-  }
-
-  get catcherOne() {
-    return this.#catcherOne;
-  }
-
-  get catcherTwo() {
-    return this.#catcherTwo;
-  }
-
-  get glitch() {
-    return this.#glitch;
-  }
-
-  get glitchPosition() {
-    return this.#glitch.position;
-  }
-
-  set glitchPosition(newPosition) {
-    this.#glitch.position = newPosition;
-  }
-
-  get catcherOnePosition() {
-    return this.#catcherOne.position;
-  }
-
-  set catcherOnePosition(newPosition) {
-    this.#catcherOne.position = newPosition;
-  }
-
-  get catcherTwoPosition() {
-    return this.#catcherTwo.position;
-  }
-
-  getCatcherScore(catcherId) {
-    return this.#score.get(catcherId).points ?? 0;
-  }
-
-  getGlitchStrike(catcherId) {
-    return this.#score.get(catcherId)?.glitchStrike ?? 0;
   }
 
 }
