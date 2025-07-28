8da5c5ac222fb62b8963cce805ec0e42c707bb0e
Irene Volodko <ice7aye@gmail.com>
2025-07-28 15:47:19 +0300

    feat(core): create PointsToWin class with mode-driven logic

---
+++ src/points-to-win.js
@@ -0,0 +1,19 @@
+export class PointsToWin {
+  constructor({ mode = 'duel', customPoints = null } = {}) {
+    this.mode = mode;
+
+    this.presets = {
+      blitz: 50,
+      duel: 150,
+      marathon: 300,
+      custom: customPoints
+    };
+  }
+
+  getPoints() {
+    if (this.mode === 'custom' && this.presets.custom == null) {
+      throw new Error('Custom points value is not set');
+    }
+    return this.presets[this.mode];
+  }
+}
