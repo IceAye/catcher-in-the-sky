d2e702a80ef8cb2757bbc1d6b90792cadcb71efe
Irene Volodko <ice7aye@gmail.com>
2025-08-17 22:39:49 +0300

    feat(settings): add static presets to skySize config class
    * Introduced predefined presets for skySize settings to improve UX and reduce manual input.

--- src/config/sky-size.js
+++ src/config/sky-size.js
@@ -1,4 +1,12 @@
 export class SkySize {
+  static presets = {
+    '4x4': 4,
+    '5x5': 5,
+    '6x6': 6,
+    '7x7': 7,
+    '8x8': 8,
+  };
+
   constructor({ columnsCount = 4 , rowsCount = 4 } = {}) {
     this.columnsCount = columnsCount;
     this.rowsCount = rowsCount;
